f867b80143193ef7b1011175274cac4af7fc71e1
csumn <csumn13@gmail.com>
2022-03-25 13:54:24 +0530

    3 Integers find the maximum

---
+++ Generics/src/com/bridgelabz/MaximumNumber.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class MaximumNumber {
+	public static Integer findMaximumNumber(Integer x, Integer y, Integer z) {
+		Integer max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		return max;
+	}
+}
