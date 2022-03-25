4a7339cc59fad75386968836733378f03b671fc2
csumn <csumn13@gmail.com>
2022-03-25 15:12:04 +0530

    parameter constructor

--- Generics/src/com/bridgelabz/FindMaximum.java
+++ Generics/src/com/bridgelabz/FindMaximum.java
@@ -3,6 +3,16 @@ package com.bridgelabz;
 public class FindMaximum <T extends Comparable<T>>{
 	T x, y, z;
 
+	public FindMaximum(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public T maximum() {
+		return FindMaximum.maximumIs(x, y, z);
+	}
+
 	public static <T extends Comparable<T>> T maximumIs(T x, T y, T z) {
 		T max = x;
 		if (y.compareTo(max) > 0) {
